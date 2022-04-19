a26e146cfe3d317df4367a49175d62431d467449
Tran Nguyen <tcdnguyen1997@gmail.com>
2022-04-19 11:35:38 +0700

    Remove rust-gmp, timeout of reqwest, use https nyc server url

--- src/utilities/requests.rs
+++ src/utilities/requests.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, Result};
 use floating_duration::TimeFormat;
 use serde;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 #[derive(Debug)]
 pub struct ClientShim {
@@ -14,7 +14,6 @@ pub struct ClientShim {
 impl ClientShim {
     pub fn new(endpoint: String, auth_token: Option<String>, user_id: String) -> ClientShim {
         let client = reqwest::blocking::Client::builder()
-            .timeout(Duration::new(u32::MAX.into(), 0))
             .build()
             .unwrap();
 
